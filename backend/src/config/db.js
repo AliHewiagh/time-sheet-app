de20da43c7a403e5d784bf269cc95af7aa4e7ac5
Alihewaigh <alihewaigh@gmail.com>
2021-07-18 13:40:02 +0800

    setup node app structure

---
+++ backend/src/config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const db = process.env.TIME_SHEET_DB;
+
+const connectDB = async () => {
+    let attempts = 10;
+
+    /* try to connect more than once in case MongoDB container takes some time to be up
+     */
+    while (attempts) {
+        try {
+            await mongoose.connect(db);
+            console.log('Time Sheet DB connected...');
+            break;
+        } catch (err) {
+            console.log('Error: ', err.message);
+            attempts -= 1;
+            console.log(`connection attempts left: ${attempts}`);
+            await new Promise((res) => setTimeout(res, 10000));
+        }
+    }
+};
+
+module.exports = { connectDB };
