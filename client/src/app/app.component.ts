3290af03e5c0e89033cfbade34aa503c378514b6
Alihewaigh <alihewaigh@gmail.com>
2021-07-21 06:24:38 +0800

    check for user authintication upon app firing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
+import { AuthService } from './services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (!user) {
+      this.authService.logout();
+    }
+
+    this.authService.storeUserToLocalStorage(user);
+  }
 }
