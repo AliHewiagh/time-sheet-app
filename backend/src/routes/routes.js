ef3f7a622caafa8a83399969b77193da80513287
Alihewaigh <alihewaigh@gmail.com>
2021-07-21 06:07:32 +0800

    create auth and event routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,3 +1,38 @@
-const routes = (app) => {};
+const { check } = require('express-validator');
+const AuthController = require('../controllers/auth.controller');
+const EventController = require('../controllers/event.controller');
+
+const requestBodyValidator = require('../middlewares/requestBodyValidator.middleware');
+const authMiddleware = require('../middlewares/auth.middleware');
+
+const routes = (app) => {
+    app.post('/v1/event', EventController.create);
+    app.get('/v1/event', authMiddleware.authorize, EventController.get);
+    app.patch('/v1/event/:id', authMiddleware.authorize, EventController.edit);
+
+    app.post(
+        '/v1/signup',
+        [
+            check('password')
+                .isLength({
+                    min: 6,
+                })
+                .withMessage('`password` should be more than 6 characters'),
+            check('password').exists().withMessage('`password` is required'),
+            check('email').exists().withMessage('`email` is required'),
+        ],
+        requestBodyValidator.check,
+        AuthController.signup
+    );
+    app.post(
+        '/v1/login',
+        [
+            check('password').exists().withMessage('`password` is required'),
+            check('email').exists().withMessage('`email` is required'),
+        ],
+        requestBodyValidator.check,
+        AuthController.login
+    );
+};
 
 module.exports = routes;
