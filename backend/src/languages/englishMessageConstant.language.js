e23667bf7d7fb465beb3f9a5d9c2a09560bc9a4d
Alihewaigh <alihewaigh@gmail.com>
2021-07-21 06:03:10 +0800

    added english transaltion

---
+++ backend/src/languages/englishMessageConstant.language.js
@@ -0,0 +1,8 @@
+module.exports = {
+    '-1': 'There seems to be a problem performing your action, please try again.',
+    '-200': 'Unauthorized',
+    '0013': 'Email is in use',
+    '0006': 'Error in Body param(s)',
+    '0009': 'Invalid token',
+    '0010': 'Expiry token',
+};
