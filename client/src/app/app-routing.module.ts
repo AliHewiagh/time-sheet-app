28a5a5054bfad42001121e17e746cfd8a1d7503e
Alihewaigh <alihewaigh@gmail.com>
2021-07-21 06:23:23 +0800

    add auth routing component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
+import { AuthGuard } from './guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./calendar/calendar.module').then(
+            (m) => m.CalendarSheetModule
+          ),
+      },
+      {
+        path: 'calendar-sheet',
+        loadChildren: () =>
+          import('./calendar/calendar.module').then(
+            (m) => m.CalendarSheetModule
+          ),
+      },
+    ],
+  },
+  /**
+   * Public Routing (Pages can be visited without authentication)
+   */
+  {
+    path: 'auth',
+    // loadChildren: './auth/auth.module#AuthModule',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
