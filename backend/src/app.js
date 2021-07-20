c742b31364d3a1e0c2232b9aa353e9fe418650db
Alihewaigh <alihewaigh@gmail.com>
2021-07-21 05:53:10 +0800

    override response object

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 
 // Express App Setup
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 const app = express();
 const { connectDB } = require('./config/db');
+
+const Response = require('./classes/response');
+const ErrorHelper = require('./helpers/error.helper');
 
 // Connect Database
 if (process.env.NODE_ENV !== 'test') {
@@ -14,6 +18,35 @@ if (process.env.NODE_ENV !== 'test') {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// allow cors
+app.use(cors());
+
+// overwrite languageId
+app.use(function (req, res, next) {
+    if (!req.header('languageId')) {
+        req.headers['languageid'] = req.header('language-id');
+    }
+    next();
+});
+// overwrite res object
+app.use(function (req, res, next) {
+    res.sendSuccess = (data, total_count) => {
+        return res.json(
+            new Response({
+                code: '0000',
+                data,
+            })
+        );
+    };
+
+    res.sendError = (code, languageId, action, devError) => {
+        return res.json(
+            ErrorHelper.getError(code, languageId, action, devError)
+        );
+    };
+    next();
+});
 
 // for testing purposes
 app.get('/test', (req, res) => {
