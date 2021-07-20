f17cc6388d67d201c32721c93c617bd457ec12b1
Alihewaigh <alihewaigh@gmail.com>
2021-07-21 05:58:16 +0800

    add user shcema

---
+++ backend/src/models/user.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    uid: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = model('User', userSchema);
+module.exports = User;
