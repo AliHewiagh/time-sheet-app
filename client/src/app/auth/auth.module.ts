39ef4c1dada589679c4ebd1aba53ec53a2da4065
Alihewaigh <alihewaigh@gmail.com>
2021-07-21 06:14:56 +0800

    add auth module and routing

---
+++ client/src/app/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [LoginComponent, SignupComponent],
+  imports: [CommonModule, AuthRoutingModule, FormsModule, ReactiveFormsModule],
+})
+export class AuthModule {}
